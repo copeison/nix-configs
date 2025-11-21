2adfd327418965844c24558dca6be8db36fa8d0e
unison <unisonsolos@gmail.com>
2025-11-21 12:30:18 -0700

    move configs around

--- systems/T430/configuration.nix
+++ systems/T430/configuration.nix
@@ -8,7 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      ./home.nix
+      home-manager/home.nix
       ./services.nix
       ./networking.nix
       ./packages.nix
