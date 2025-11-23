c4a6b1381a62126a95c033ff5de7ed54f8c6a196
unison <unisonsolos@gmail.com>
2025-11-23 16:21:48 -0700

    update configs for optiplex

--- systems/optiplex/home-manager/home.nix
+++ systems/optiplex/home-manager/home.nix
@@ -21,24 +21,10 @@ in
     home.file.".config/zsh/omz-custom".source = ./config/omz-custom;
     home.file.".config/zsh/omz-custom".recursive = true;
 
-  gtk = {
-      enable = true;
-      gtk3.extraConfig.gtk-application-prefer-dark-theme = 1;
-      iconTheme = {
-        name = "Papirus-Dark";
-        package = pkgs.papirus-icon-theme;
-      };
-      theme = {
-        name = "Adwaita-dark";
-        package = pkgs.gnome-themes-extra;
-      };
-    };
-
     home.username = "ethan";
     home.homeDirectory = "/home/ethan";
 
     home.packages = [
-      pkgs.filezilla
     ];
 
     # The state version is required and should stay at the version you
