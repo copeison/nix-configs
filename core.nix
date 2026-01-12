399a619307be08eb0ce56c9d29baca8ed2f830b0
Vali0004 <diorcheats.vali@gmail.com>
2026-01-12 01:50:08 -0500

    Remove my personal SSH keys

--- core.nix
+++ core.nix
@@ -1,7 +1,5 @@
 let
-  keys = (import ./ssh_keys_personal.nix) ++ [
-    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDJR3qDc8r2kbg6Q+A0dk7E6fC/wdlySBKb8X+8XgRGJg6huXaCTPZbAyvzt1IvxY69IdBymExjUie7YuFOLOKi5wisfw6d1yVjrhaoZWvXTz6eyF0ssAzM1BbqJsHU2dahQnNo7ThUguR365woBaw1UrZHEjlAiX16NxDVEyaXNImDjlQKBiAyDaa/pOCe1GUYwPgXHJMwF+6JbY+pGYAm6AvvsnjhLO0kyzwv1hSOd4qlzSobkDE9FQMbJD7uV+D1cXAv2ERdf/h9/L5dUcOEUscES+wg8ezLOhaBmq8TT9K3gmhMa47zNQU1WUAg39n+2+/Dwix0j7GNsNZdbp6B"
-  ];
+  keys = (import ./ssh_keys_personal.nix);
 in {
   console.keyMap = "us";
 
