817dd56af6cced4dc3379245ca1c2072a7418113
copeison <184175589+copeison@users.noreply.github.com>
2026-03-07 19:19:32 -0700

    new config for t430

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,11 +1,9 @@
 let
   ssh_keys = import ../ssh_keys_personal.nix;
-  T430 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKK+d1tHVMKqyed0hX3cctelxk7+vPknhuIC7pgm+UmQ";
   T480s = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKQwUD3mL7KyuHS23ZqF9txhboffERewfGDzYKQ8fplg";
   r33-local = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICnauTgmSibFaafYvDNr5pZF9daFLVrl7cfsxZA5D+sQ";
   shittyvps = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG0fCV7QcBNz5iLh2rMtlyGoAAr8nrYC8P68TkhZtTyq";
   keys = ssh_keys ++ [
-    T430
     T480s
   ];
 
